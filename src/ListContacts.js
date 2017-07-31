6b002a97fcfceaf1ace573808df0018b6a237cda
Jesus Amaya <jesus.amaya@rulesware.com>
2017-07-31 16:50:28 -0600

    controlled components 1

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -1,30 +1,48 @@
-import React from 'react'
+import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 
 
-function ListContacts(props){
-	return (
-		<ol className='contact-list'>
-			{props.contacts.map(contact=> (
-				<li key={contact.id} className='contact-list-item'>
-					<div className='contact-avatar' style={{
-						backgroundImage: `url(${contact.avatarURL})`
-					}}/>
-					<div className='contact-details'>
-						<p>{contact.name}</p>
-						<p>{contact.email}</p>
-					</div>
-					<button onClick={()=> props.onContactDelete(contact)} className='contact-remove'>
-						Remove
-					</button>
-				</li>))
-			}
-		</ol>
-	)
-}
-ListContacts.propTypes = {
-	contacts : PropTypes.array.isRequired
-	,onContactDelete : PropTypes.func.isRequired
+class ListContacts extends Component {
+	static propTypes = {
+		contacts : PropTypes.array.isRequired
+		,onContactDelete : PropTypes.func.isRequired
+	}
+	state = { query:'' }
+
+	updateQuery = (query)=>{
+		this.setState({ query: query.trim()})
+	}
+	render(){
+		return (
+			<div className='list-contacts'>
+				<div className='list-contacts-top'>
+					<input
+						className='search-contacts'
+						type='text'
+						placeholder='Search contacts'
+						value={this.state.query}
+						onChange={(event)=>this.updateQuery(event.target.value)}
+					/>
+				</div>
+				<ol className='contact-list'>
+					{this.props.contacts.map(contact=> (
+						<li key={contact.id} className='contact-list-item'>
+							<div className='contact-avatar' style={{
+								backgroundImage: `url(${contact.avatarURL})`
+							}}/>
+							<div className='contact-details'>
+								<p>{contact.name}</p>
+								<p>{contact.email}</p>
+							</div>
+							<button onClick={()=> this.props.onContactDelete(contact)} className='contact-remove'>
+								Remove
+							</button>
+						</li>))
+					}
+				</ol>
+			</div>
+		)
+	}
 }
 
 export default ListContacts;
